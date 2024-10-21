6c498f9d66fbd5bd97297d5ec5c948ca25d3e6cf
jc4720 <jc4720@imperial.ac.uk>
2024-10-22 00:04:30 +0100

    lab3

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def process_query(query):
         return "Dinosaurs ruled the Earth 200 million years ago"
     elif query == "asteroids":
         return "Unknown"
+    else:
+        return "Unknown"
 
 
 @app.route("/query", methods=["GET"])
